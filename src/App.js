e3b244e4b230e69bde6b2aa1af354f5de9bf6b41
Nicolas Vargas <njvnicovargas@gmail.com>
2022-07-27 17:04:28 -0300

    eco commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Eco Barbell Club 
         </p>
         <a
           className="App-link"
