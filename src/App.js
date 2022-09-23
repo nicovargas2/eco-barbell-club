2c62b9ae516d0dd96920aab6ccf4fee2062decd7
Nicolas Vargas <njvnicovargas@gmail.com>
2022-09-23 17:29:11 -0300

    5v-color y estilo de entrenadores, correcion links de footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import './App.css';
 import React from 'react';
-import { useEffect } from 'react';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import Events from './components/pages/Events';
 import TrainWithUs from './components/pages/TrainWithUs';
 
-
-
 function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/trainwithus" element={<TrainWithUs />} />        
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
+
+/*
+
   // Redirect Page
 const RedirectPage1 = () => {
   useEffect(() => {
@@ -32,35 +48,6 @@ const RedirectPage2 = () => {
   </div>
 }
 
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/trainwithus" element={<TrainWithUs />} />
         <Route path='/external-link-1' element={<RedirectPage1 />} />
         <Route path='/external-link-2' element={<RedirectPage2 />} />
-        
-        </Routes>
-      </Router>
-    </>
-  );
-}
-
-export default App;
-
-/*
-hacer fondo como la de home, para train with us
-y poner la imagen grande de ECO
-
-ahi poner la filosofia y demas, hacer un cuadro con transparencia,
-y letras a tono
-
-abajo poner fotos de los 3 entrenadores con detalles
-fondo a determinar
-
---
-en el footer meter logo de pitbull certified
 */
